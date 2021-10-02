347c679e19b7c32b2a417c79c3b7f39a1ddf39a6
jartigag <jartigag@pm.me>
2021-10-02 12:30:47 +0200

    init: html structure, #wip css

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="card">
+        <div className="title">
+          <h1 className="bold name">Bulbasaur</h1>
+          <h2 className="bold number">#001</h2>
+        </div>
+        <img className="image" src="" alt=""/>
+        <div className="card-content">
+        <div class="tags-row">
+          <div className="bold tags">
+              <p>Grass</p>
+          </div>
+          <div className="bold tags">
+              <p>Poison</p>
+          </div>
+        </div>
+        <a className="bold about" href="/#">About</a>
+        <div className="measures">
+            <p className="weight">6,9 kg</p>
+            <p className="height">0,7 m</p>
+        </div>
+        <div className="description">
+            <p>There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.</p>
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
