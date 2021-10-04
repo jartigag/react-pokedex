a63507e122dd865224a173fa6fbbcafbdb95c990
jartigag <jartigag@pm.me>
2021-10-04 20:41:19 +0200

    bem notation completed

--- src/App.js
+++ src/App.js
@@ -3,28 +3,28 @@ import './App.css';
 function App() {
   return (
     <div className="card">
-        <div className="title">
-          <h1 className="bold name">Bulbasaur</h1>
-          <h2 className="bold number">#001</h2>
+        <div className="card__header">
+          <h2 className="bold card__name">Bulbasaur</h2>
+          <span className="bold card__number">#001</span>
         </div>
-        <img className="image" src="img/bulbasaur.png" alt=""/>
+        <img className="card__image" src="img/bulbasaur.png" alt=""/>
         <div className="card__content">
-        <div class="tags">
-          <div className="bold tag">
+        <div className="card__tags">
+          <div className="bold card__tag bg-grass">
               <span>Grass</span>
           </div>
-          <div className="bold tag">
+          <div className="bold card__tag bg-poisson">
               <span>Poison</span>
           </div>
         </div>
-        <a className="bold about" href="/#">About</a>
-        <div className="measures">
+        <a className="bold card__about" href="/#">About</a>
+        <div className="card__measures">
             <img src="icon/weight_icon.svg" alt="" />
             <span className="weight">6,9 kg <span class="measure__name">Weight</span></span>
             <img src="icon/height_icon.svg" alt="" />
             <span className="height">0,7 m <span class="measure__name">Height</span></span>
         </div>
-        <div className="description">
+        <div className="card__description">
             <p>There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.</p>
         </div>
         </div>
